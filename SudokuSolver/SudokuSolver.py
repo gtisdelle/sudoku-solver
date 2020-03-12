50fa9128e3243f3602276dcc435e23bf114e421d
George Tisdelle <tisdelgw@dukes.jmu.edu>
2020-03-12 17:02:02 -0400

    Change comment

--- SudokuSolver/SudokuSolver.py
+++ SudokuSolver/SudokuSolver.py
@@ -2,18 +2,6 @@
  This sudoku solver uses a backtracking algorithm. It was developed to help me
  further learn python.
  
- Sudoku is a classic constraint satisfaction problem and contains a
- partial candidate solution where it can be quickly and trivialy determined
- whether it can possibly be completed into a valid solution.
- 
- I was inspired to solve the problem for a sudoku board and attained most of
- my understanding of a backtracking algorithm from the Wikipedia page on
- backtracking.
- 
- Currently the puzzle is hard coded to be the sudoku puzzle from
- the New York Times on February 1, 2020, but I'm planning to exapand
- it to use an API to generate puzzles.
- 
  Author: George Tisdelle.
 '''
 
